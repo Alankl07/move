254e452e0b72b0aca578e6b1d6139c853e6322d3
lasilva <lucas.almeida.las@gmail.com>
2018-12-12 21:28:55 -0300

    Adicionado o querido projeto

---
+++ move/notificacao/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Setor(models.Model):
+    nome = models.CharField(max_length=60, null=False)
+    esferaPublica = models.CharField(max_length=1, null=False,
+                                     default='M')
